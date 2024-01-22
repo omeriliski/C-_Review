dd3b331feec418798b9e1782be9c7a1db642d66c
Ömer Iliski <omer.iliski@ditegra.de>
2024-01-22 15:16:08 +0100

    added last changes

--- C#_Basics/EmptyProject/Program.cs
+++ C#_Basics/EmptyProject/Program.cs
@@ -1,6 +1,14 @@
+using C__Basics.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+Product product1 = new Product(1, "TV", 1000);
+Product product2 = new Product(2, "Phone", 2000);
+Product product3 = new Product(3, "Computer", 3000);
+
+List<Product> products = new List<Product>() { product1, product2, product3 };
+
+app.MapGet("/api/products", () => products);
 
 app.Run();
--- C#_Basics/Web_Api/Controllers/TestController.cs
+++ C#_Basics/Web_Api/Controllers/TestController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using C__Basics.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Web_Api.Controllers
 {
@@ -7,9 +9,15 @@ namespace Web_Api.Controllers
     public class TestController: ControllerBase     // write hier the name of the controller
     {
         [HttpGet(Name = "GetTest")]
-        public string Get()
+        public ActionResult<IEnumerable<Product>> Get()
         {
-            return "lorem ipsum";
+            Product product1 = new Product(1, "TV", 1000);
+            Product product2 = new Product(2, "Phone", 2000);
+            Product product3 = new Product(3, "Computer", 3000);
+
+            List<Product> products = new List<Product>() { product1, product2, product3 };
+            //var productsJson = JsonConvert.SerializeObject(products);
+            return Ok(products);
         }
     }
 }
